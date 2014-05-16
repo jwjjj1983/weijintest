4438f09bbe28c15b3e7d6fccd156a73cd6ea453f
sarahxh <sarahxh@gmail.com>
2014-05-15 22:55:38 -0700

    Update java version and LinkedList

--- list/src/list/LinkedList.java
+++ list/src/list/LinkedList.java
@@ -2,13 +2,21 @@ package list;
 
 public class LinkedList implements List{
     private Node head;
+    private Node tail;
     
     public LinkedList(){
         
     }
     
     public void add(Object o){
-        
+        Node n = new Node(o);
+        if(tail != null){
+            tail.next = n;
+        }
+        tail = n;
+        if(head == null){
+            head = n;
+        }
     }
     
     public void addFirst(Object o){
@@ -57,11 +65,46 @@ public class LinkedList implements List{
     }
     
     public static void main(String args[]){
-        
+              // create a linked list
+      LinkedList ll = new LinkedList();
+      // add elements to the linked list
+      ll.add("F");
+      ll.add("B");
+      ll.add("D");
+      ll.add("E");
+      ll.add("C");
+      ll.addLast("Z");
+      ll.addFirst("A");
+      ll.add(1, "A2");
+      System.out.println("Original contents of ll: " + ll);
+
+      // remove elements from the linked list
+      ll.remove("F");
+      ll.remove(2);
+      System.out.println("Contents of ll after deletion: "
+       + ll);
+      
+      // remove first and last elements
+      ll.removeFirst();
+      ll.removeLast();
+      System.out.println("ll after deleting first and last: "
+       + ll);
+
+      // get and set a value
+      Object val = ll.get(2);
+      ll.set(2, (String) val + " Changed");
+      System.out.println("ll after change: " + ll);
     }
 }
 
 class Node {
     Object o;
     Node next;
+    public Node(Object o) {
+        this.o = o;
+    }
+    public Node(Object o, Node next) {
+        this.o = o;
+        this.next = next;
+    }
 }
